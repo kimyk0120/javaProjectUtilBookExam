7f651faef3b43d24aabf1d01b2f7ab85a8b25f11
kimyk0120 <kimyk0120@hotmail.com>
2018-10-04 10:28:33 +0900

    Signed-off-by: kimyk0120 <kimyk0120@hotmail.com>

--- eclipse-maven-app/src/main/java/com/tuyano/eclipse_maven_app/App.java
+++ eclipse-maven-app/src/main/java/com/tuyano/eclipse_maven_app/App.java
@@ -8,6 +8,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new App().printMessage("YK");
     }
+    
+    public String getMessage(String name){
+    	return "Maven world , " + name;
+    }
+    
+    public void printMessage(String name){
+    	System.out.println("App Class");
+    	System.out.println(this.getMessage(name));
+    	System.out.println(".App Class");
+    }
+    
 }
